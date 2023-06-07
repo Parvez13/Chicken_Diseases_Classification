c031d8f3074bb8fc927081d9de047c27ca657992
Parvez13 <prvzsohail@gmail.com>
2023-06-07 10:03:39 +0530

    Stage_02_prepare_base_model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from chicken_disease.logger import logging 
 from chicken_disease.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from chicken_disease.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -10,3 +11,15 @@ try:
 except Exception as e:
     logging.exception(e)
     raise e
+
+STAGE_NAME = "Prepare base model"
+try:
+   logging.info(f"*******************")
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   obj = PrepareBaseModelTrainingPipeline()
+   obj.main()
+   logging.info(
+      f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logging.exception(e)
+   raise e
